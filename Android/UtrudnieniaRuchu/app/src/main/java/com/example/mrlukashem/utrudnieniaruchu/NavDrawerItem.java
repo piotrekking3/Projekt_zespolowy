93991867bb8386011d468b46c0249994fb10dc4b
Robert <robek.chojnacki@gmail.com>
2015-03-20 13:11:14 +0100

    Nowy branch dla aplikacji webowej

---
+++ Android/UtrudnieniaRuchu/app/src/main/java/com/example/mrlukashem/utrudnieniaruchu/NavDrawerItem.java
@@ -0,0 +1,11 @@
+package com.example.mrlukashem.utrudnieniaruchu;
+
+/**
+ * Created by mrlukashem on 19.03.15.
+ */
+
+public interface NavDrawerItem {
+    public int getId();
+    public String getLabel();
+    public int getType();
+}
