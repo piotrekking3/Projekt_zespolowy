c44f2ce9916041425de4e88bacb3688efd984710
MrLukashem <mertalukasz@gmail.com>
2015-04-03 11:33:05 +0200

    Modyfikacja kolejnosci dla dodawania nowego markera
    -kosmetyczna zmiana interfejsu NewMarkerOnMap
    -MainActivity odpala fragment dialogi za pomoca metod, a nie
    bezposrednio w switchu
    -teraz najpierw wyznaczamy miejsce na mapie, a potem dopiero wypelniamy
    prototypowy formularz danych(taka kolejnosc jak na web app)
    
    Change-Id: Ia145883c867b3c34cede05699d9a09fa5deec224

--- Android/UtrudnieniaRuchu/app/src/main/java/com/example/mrlukashem/utrudnieniaruchu/NewMarkerOnMap.java
+++ Android/UtrudnieniaRuchu/app/src/main/java/com/example/mrlukashem/utrudnieniaruchu/NewMarkerOnMap.java
@@ -13,7 +13,7 @@ import android.provider.ContactsContract;
     formularz służący do pobrania danych od użytkownika.
  */
 public interface NewMarkerOnMap {
-    void setLongClickListener();
     void setToastMarkerInfo();
-    void setData(String __email, int __category_id, String __content);
+    void addMarkerFromFormData();
+    void sendFormData(String __email, int __category_id, String __content);
 }
