781b3addc97beab2e2f73c5d1bbb9f4e388d1fee
Piotr Kulewicz <piotrekking3@wp.pl>
2015-04-14 17:30:17 +0200

    Pobieranie markerow przez RESTapi
    Change-Id: I7e41f4f5b31f31b0bcd9453dcb4c0f1375285911

--- www/scripts/wmb.markers.js
+++ www/scripts/wmb.markers.js
@@ -1,10 +1,60 @@
-// Definicje znaczników
-// Ten plik powinien być generowany przez PHP na podstawie punktów zapisanych w bazie danych
+// Definicje znaczników i ikon
+var redIcon = L.icon({
+	iconUrl: 'scripts/images/marker_red.png',
+	shadowUrl: 'scripts/images/marker-shadow.png',
+	iconSize:     [21, 34],
+	shadowSize:   [0, 0],
+	iconAnchor:   [10, 17],
+	shadowAnchor: [0, 0],
+	popupAnchor:  [0, 0]
+});
 
-L.marker([51.110121, 17.032020], {icon: redIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 1").addTo(category1);
-L.marker([51.110902, 17.032225], {icon: greenIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 2").addTo(category2);
-L.marker([51.110000, 17.032425], {icon: blueIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 3").addTo(category3);
-L.marker([51.109200, 17.030254], {icon: purpleIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 5").addTo(category5);
-L.marker([51.109950, 17.030954], {icon: yellowIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 4").addTo(category4),
-L.marker([51.111150, 17.030444], {icon: yellowIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 4").addTo(category4),
-L.marker([51.109255, 17.011154], {icon: yellowIcon}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 4").addTo(category4);
+var greenIcon = L.icon({
+	iconUrl: 'scripts/images/marker_green.png',
+	shadowUrl: 'scripts/images/marker-shadow.png',
+	iconSize:     [21, 34],
+	shadowSize:   [0, 0],
+	iconAnchor:   [10, 17],
+	shadowAnchor: [0, 0],
+	popupAnchor:  [0, 0]
+});
+
+var blueIcon = L.icon({
+	iconUrl: 'scripts/images/marker_blue.png',
+	shadowUrl: 'scripts/images/marker-shadow.png',
+	iconSize:     [21, 34],
+	shadowSize:   [0, 0],
+	iconAnchor:   [10, 17],
+	shadowAnchor: [0, 0],
+	popupAnchor:  [0, 0]
+});
+
+var yellowIcon = L.icon({
+	iconUrl: 'scripts/images/marker_yellow.png',
+	shadowUrl: 'scripts/images/marker-shadow.png',
+	iconSize:     [21, 34],
+	shadowSize:   [0, 0],
+	iconAnchor:   [10, 17],
+	shadowAnchor: [0, 0],
+	popupAnchor:  [0, 0]
+});
+
+var purpleIcon = L.icon({
+	iconUrl: 'scripts/images/marker_purple.png',
+	shadowUrl: 'scripts/images/marker-shadow.png',
+	IconSize:     [21, 34],
+	shadowSize:   [0, 0],
+	iconAnchor:   [10, 17],
+	shadowAnchor: [0, 0],
+	popupAnchor:  [0, 0]
+});
+
+var cat = {1:category1, 2:category2, 3:category3, 4:category4}
+var colorIcon = {1:redIcon, 2:greenIcon, 3:blueIcon, 4:yellowIcon, 5:purpleIcon}
+
+$.getJSON("http://virt2.iiar.pwr.edu.pl/api/zgloszenia/getAll",
+	function(data){
+		$.each(data, function(i){
+			L.marker([data[i].zgloszenia.wspolrzedne.x, data[i].zgloszenia.wspolrzedne.y], {icon: colorIcon[data[i].zgloszenia.id_typu]}).bindPopup("<b>Wrocław - Rynek</b><br>Kategoria 4").addTo( cat[data[i].zgloszenia.id_typu] );
+		});
+});
