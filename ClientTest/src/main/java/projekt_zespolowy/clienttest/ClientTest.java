92c89994044d0ebd97effddde95bddcf97758cac
Piotrek <malfoniusz@gmail.com>
2015-04-17 06:20:52 +0200

    Zaktualizowana wersja RESTa
    Change-Id: I951ce562639c9e07a473c65e4cca7a074ecf2b15

---
+++ ClientTest/src/main/java/projekt_zespolowy/clienttest/ClientTest.java
@@ -0,0 +1,69 @@
+package projekt_zespolowy.clienttest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import org.codehaus.jettison.json.JSONObject;
+
+public class ClientTest
+{
+    public static void main(String[] args) {
+        int i = 1;
+		String input;
+        String[] string = {
+            "{\n"
+            + "\"zgloszenia\": {\n"
+            + " \"id_uzytkownika\": 1,\n"
+            + " \"id_typu\": 1, \n"
+            + " \"id_disqus\": 5, \n"
+            + " \"wspolrzedne\": {\n"
+            + "     \"type\":\"point\",\"value\":\"(51.094703, 17.021475)\", \"x\":51.094703, \"y\":17.021475\n"
+            + " }\n"
+            + "}\n"
+            + "}",
+            "{\n"
+            + "\"uzytkownicy\": {\n"
+            + " \"nick\": \"nick5\","
+            + " \"email\": \"email5@email.com\","
+            + " \"haslo\": \"haslo\""
+            + " }\n"
+            + "}"
+        };
+
+        try {
+            JSONObject jsonObject = new JSONObject(string[i]);
+
+            try {
+                URL[] url = {
+                    new URL("http://localhost:8080/RestApi/service/zgloszenia/post"),
+                    new URL("http://localhost:8080/RestApi/service/uzytkownicy/post")
+                };
+                URLConnection connection = url[i].openConnection();
+
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+                OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+                out.write(jsonObject.toString());
+                out.close();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                while ((input = in.readLine()) != null) {
+					System.out.println(input);
+                }
+                System.out.println("\nSukces");
+                in.close();
+            } catch (Exception e) {
+                System.out.println("\nNie przeszlo");
+                System.out.println(e);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
---
+++ RestAPI/src/main/java/projekt_zespolowy/restapi/services/ServiceZgloszenia.java
@@ -0,0 +1,80 @@
+package projekt_zespolowy.restapi.services;
+
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import projekt_zespolowy.restapi.dao.ZgloszeniaDao;
+import projekt_zespolowy.restapi.model.Zgloszenia;
+
+@Path("/service/zgloszenia")
+public class ServiceZgloszenia
+{
+    private ZgloszeniaDao zgloszeniaDao = new ZgloszeniaDao();
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Zgloszenia> getAll() {
+        return zgloszeniaDao.getAll();
+    }
+
+    @GET
+    @Path("/getById/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Zgloszenia getById(@PathParam("id") int id) {
+        return zgloszeniaDao.getById(id);
+    }
+
+    @GET
+    @Path("/getByType/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Zgloszenia> getByType(@PathParam("id") int id) {
+        return zgloszeniaDao.getByType(id);
+    }  
+
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(String incomingData) {
+        Zgloszenia zgloszenia = new Zgloszenia();
+        int httpCode = 500, idx = 0, oldIdx = 0;
+
+        try {
+            if ((idx = zgloszenia.setId_uzytkownikaByJSON(incomingData, idx)) == oldIdx) {
+                throw new Exception();
+            }
+            oldIdx = idx;
+            if ((idx = zgloszenia.setId_typuByJSON(incomingData, idx)) == oldIdx) {
+                throw new Exception();
+            }
+            oldIdx = idx;
+            if ((idx = zgloszenia.setId_disqusByJSON(incomingData, idx)) == oldIdx) {
+                throw new Exception();
+            }
+            oldIdx = idx;
+            if (zgloszenia.setWspolrzedneByJSON(incomingData, idx) == oldIdx) {
+                throw new Exception();
+            }
+        } catch (Exception ex) {
+            System.out.println("Niepowodzenie odczytania danych przychodzacych");
+        }
+
+        try {
+            httpCode = zgloszeniaDao.postZgloszenia(zgloszenia);
+        } catch (Exception ex) {
+
+        }
+
+        if (httpCode == 200) {
+            return Response.status(httpCode).entity("Operacja dodania zgloszenia zostala zakonczona sukcesem").build();
+        }
+
+        return Response.status(httpCode).entity("Operacja dodania zgloszenia nie powiodla sie").build();
+    }
+}
